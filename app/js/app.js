70fdbbcbe72c60743646dddc29ce13fff2a8d204
Dong Dong <dong.dong@iona-solutions.com>
2014-06-04 16:50:31 -0300

    init

---
+++ app/js/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var timesheetsApp = angular.module('timesheetsApp', [
+  'ngRoute',
+  'timesheetsApp.filters',
+  'timesheetsApp.services',
+  'timesheetsApp.directives',
+  'timesheetsApp.controllers'
+]);
+
+timesheetsApp.config(['$routeProvider', 
+	function($routeProvider) {
+  		$routeProvider.
+	  		when('/login', {
+	  			templateUrl: 'partials/login.html', 
+	  			controller: 'LoginController'
+	  		}).
+	  		when('/main', {
+	  			templateUrl: 'partials/main.html', 
+	  			controller: 'MainController'
+	  		})
+	  		.otherwise({redirectTo: '/login'});
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Controllers */
+
+var timesheetsControllers = angular.module('timesheetsApp.controllers', []);
+
+timesheetsControllers.controller('LoginController', ['$scope', function($scope) {
+
+}]);
+
+timesheetsControllers.controller('MainController', ['$scope', function($scope) {
+
+}]);
